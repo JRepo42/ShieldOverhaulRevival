df170f31b6c266ffbc7c7fd530581cda12ee1956
JRepo42 <JRepo42@users.noreply.github.com>
2024-09-08 15:46:00 +0100

    Parry sound working

--- src/main/java/elocin/shield_overhaul/mixin/ParryDamageMixin.java
+++ src/main/java/elocin/shield_overhaul/mixin/ParryDamageMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.sound.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,6 +22,11 @@ public class ParryDamageMixin {
 		if (source.isIn(DamageTypeTags.BYPASSES_EFFECTS)) return;
 
 		if (ShieldUtils.isParrying(player.getMainHandStack(), player) || ShieldUtils.isParrying(player.getOffHandStack(), player)) {
+			if (source.getAttacker() != null) {
+				// stun attacker
+			}
+
+			player.getWorld().playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.BLOCK_ANVIL_LAND, player.getSoundCategory(), 1.0f, 2.0f);
 			cir.setReturnValue(0f);
 		}
 	}
