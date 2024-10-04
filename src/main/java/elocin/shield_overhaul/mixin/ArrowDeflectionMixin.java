48b91a3e1a6c2402d5ac03de8d35643e5c5766e8
JRepo42 <JRepo42@users.noreply.github.com>
2024-10-04 19:01:59 +0100

    Arrow deflect fully working

--- src/main/java/elocin/shield_overhaul/mixin/ArrowDeflectionMixin.java
+++ src/main/java/elocin/shield_overhaul/mixin/ArrowDeflectionMixin.java
@@ -2,7 +2,6 @@ package elocin.shield_overhaul.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import elocin.shield_overhaul.ShieldOverhaul;
 import elocin.shield_overhaul.config.server.ShieldConfig;
 import elocin.shield_overhaul.util.ShieldUtils;
 import net.minecraft.entity.player.PlayerEntity;
@@ -10,7 +9,6 @@ import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(PersistentProjectileEntity.class)
@@ -23,6 +21,9 @@ public abstract class ArrowDeflectionMixin {
         if (!ShieldConfig.INSTANCE.arrow_deflect_requires_parry && (player.isBlocking() || ShieldUtils.isParrying(ShieldUtils.getParryStack(player), player))
             || (ShieldConfig.INSTANCE.arrow_deflect_requires_parry && ShieldUtils.isParrying(ShieldUtils.getParryStack(player), player))) {
             original.call(instance, player.getRotationVector().multiply(ShieldConfig.INSTANCE.arrow_deflect_velocity_multiplier));
+            return;
         }
+
+        original.call(instance, vec3d);
     }
 }
